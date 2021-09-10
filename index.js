932c3199fe7021935964b9e2c983137ad2024c40
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-09-10 12:39:22 +0530

    nav

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu_btn = document.querySelector(".menu-icon");
+const close_btn = document.querySelector(".close-icon");
+const menu = document.querySelector(".nav-list");
+menu_btn.addEventListener("click", () => {
+    close_btn.classList.remove("hidden");
+    menu.classList.remove("hidden");
+});
+close_btn.addEventListener("click", () => {
+    close_btn.classList.add("hidden");
+    menu.classList.add("hidden");
+});
