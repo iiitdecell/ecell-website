7b87e73427ac28d0e815b8b13bbdf2bb273438b0
sc0rp10n-ubuntu-laptop <pragyan52yadav@gmail.com>
2021-09-24 01:37:52 +0530

    form merged, footer underdev

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ close_btn.addEventListener("click", () => {
     close_btn.classList.add("hidden");
     menu.classList.add("hidden");
 });
+
+var form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("form")),
+    })
+        .then((response) => response.json())
+        .then((html) => {
+            // you can put any JS code here
+            alert("success");
+        });
+});
